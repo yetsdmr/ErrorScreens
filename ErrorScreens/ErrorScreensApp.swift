408b263f74ea869967380e47d1790be7a14d51f9
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-11-13 20:36:56 +0300

    Initial Commit

---
+++ ErrorScreens/ErrorScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorScreensApp.swift
+//  ErrorScreens
+//
+//  Created by Yunus Emre Taşdemir on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ErrorScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
